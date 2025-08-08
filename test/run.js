dc2d7d2b7465f14966243be7c59ea152a9ab6d0f
Mikko Moilanen <mikko.moilanen@suoratyo.fi>
2025-08-08 12:09:33 +0300

    Dart Sass 3.0.0 support

--- test/run.js
+++ test/run.js
@@ -1,9 +1,9 @@
-'use strict';
 
-var sassTrue = require('sass-true');
-var glob = require('glob');
+const path = require('path');
+const sassTrue = require('sass-true');
+const glob = require('glob');
 
-// Run tests
-glob.sync('test/_*.scss').forEach(function (file) {
-  sassTrue.runSass({ file: file }, { describe, it });
+// Run tests using the new True v7+ API
+glob.sync('test/_*.scss').forEach((file) => {
+  sassTrue.runSass({ describe, it }, path.resolve(file));
 });
